0b5cf2fed182b290c8abfe913bf13d838943b1f4
kloudCoder <dev@ezyy.cloud>
2024-08-22 19:57:51 +0200

    Updated config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // config.ts
 
-export const API_URL = 'http://buzzfiapi.af-south-1.elasticbeanstalk.com/api';
+export const API_URL = 'https://buzzfiapi.af-south-1.elasticbeanstalk.com/api';
 export const TOKEN = '1|jRjo0HahuhKLEIu36lJDJFEHMc94YGn2zpLybHLx46c02eff';
 
